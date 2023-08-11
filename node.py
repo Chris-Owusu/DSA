cf4f246736c79352a413bcb3e22ee58a7fd67f6c
Chris-Owusu <christopher.owusu@amalitech.com>
2023-08-11 09:52:37 +0000

    initial linkedlist commit

--- node.py
+++ node.py
@@ -29,3 +29,23 @@ wackos_data = dot.get_link_node().get_value()
 
 print(dots_data)
 print(wackos_data)
+
+# Another Example
+# Define your Node class below:
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+my_node = Node(44)
+
+print(my_node.get_value())
