4ca469680d855442e3242cb1c831e9728dec96a7
Chris-Owusu <christopher.owusu@amalitech.com>
2023-10-23 16:14:25 +0000

    queue

---
+++ queue1.py
@@ -0,0 +1,60 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+  
+  def get_value(self):
+    return self.value
+  
+
+class Queue:
+  # Add max_size and size properties within __init__():
+  def __init__(self, max_size = None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size 
+    self.size = 0
+    
+  def peek(self):
+    if self.is_empty():
+      print("Nothing to see here!")
+    else: 
+      return self.head.get_value()
+  
+  # Define get_size() and has_space() below:
+  def get_size(self):
+    return self.size
+
+  def has_space(self):
+    if self.max_size == None:
+      return True
+    return self.max_size > self.get_size()
+
+  def is_empty(self):
+    if self.size == 0:
+      return True
+    return False
+  
+
+  # Enqueue
+  def enqueue(self, value):
+    self.value = value
+
+    if self.has_space() == True:
+       item_to_add = Node(value)
+       print("Adding "  + str(item_to_add.get_value()) + " to the queue!")
+
+       if self.is_empty() == True:
+        head = item_to_add
+        tail = item_to_add
+       else:
+        tail.set_next_node(item_to_add)
+        tail = item_to_add
+
+        self.size = self.size + 1
