5fe4fd9b3aef80e97965e1703225192564a0a8cf
Chris-Owusu <christopher.owusu@amalitech.com>
2023-10-26 12:01:21 +0000

    Recursive Data Structures

--- recurssion.py
+++ recurssion.py
@@ -70,3 +70,51 @@ def flatten(my_list):
 planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uranus', ['neptune', 'pluto']]
 
 print(flatten(planets))
+
+# fibonacci
+
+def fibonacci(n): 
+  if n is 1:
+    return 1
+  elif n is 0:
+    return 0
+  
+  return fibonacci(n - 2) + fibonacci(n - 1)
+  print(n)
+
+
+
+fibonacci(5)
+# set the appropriate runtime:
+# 1, logN, N, N^2, 2^N, N!
+fibonacci_runtime = "2^N"
+
+
+# Recursive Data Structures
+
+# Define build_bst() below...
+def build_bst(my_list):
+  if my_list == []:
+    return "No Child"
+  
+  middle_idx = len(my_list) // 2
+  middle_value = my_list[middle_idx]
+  print("Middle index: {0}".format(middle_idx))
+  print("Middle value: {0}".format(middle_value))
+
+  tree_node = {"data": middle_value}
+  tree_node["right_child"] = build_bst(my_list[middle_idx + 1:])
+  tree_node["left_child"] = build_bst(my_list[ : middle_idx])
+  return tree_node
+
+
+  
+
+# For testing
+sorted_list = [12, 13, 14, 15, 16]
+binary_search_tree = build_bst(sorted_list)
+print(binary_search_tree)
+
+# fill in the runtime as a string
+# 1, logN, N, N*logN, N^2, 2^N, N!
+runtime = "O(N*logN)"
