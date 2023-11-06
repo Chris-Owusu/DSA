8841261302b901440dcd1192c05b153b76a348c1
Chris-Owusu <christopher.owusu@amalitech.com>
2023-11-06 10:01:53 +0000

    routing program final

--- routingProgram.py
+++ routingProgram.py
@@ -154,6 +154,12 @@ def dfs(graph, current_vertex, target_value, visited = None):
         return path
 
 # Build your program below:
+from graph_search import bfs, dfs
+from vc_metro import vc_metro
+from vc_landmarks import vc_landmarks
+from landmark_choices import landmark_choices
+
+# Build your program below:
 landmark_string = ""
 def greet():
   print("Hi there and welcome to SkyRoute!")
@@ -161,6 +167,9 @@ def greet():
 
 def skyroute():
   greet()
+  new_route()
+  goodbye()
+
 
 def get_start():
   start_point_letter = input("Where are you coming from? Type in the corresponding letter: ")
@@ -198,9 +207,39 @@ def set_start_and_end(start_point, end_point):
     end_point = get_end()
   return start_point, end_point
 
+def new_route(start_point=None, end_point=None):
+  start_point, end_point = set_start_and_end(start_point, end_point)
+  shortest_route = get_route(start_point, end_point)
+  shortest_route_string = '\n'.join(shortest_route)
+  print("The shortest metro route from {0} to {1} is:\n{2}".format(start_point, end_point, shortest_route_string))
+  again = input("Would you like to see another route? Enter y/n: ")
+  if again == "y":
+    new_route(start_point, end_point)
+    show_landmarks()
+  
+def show_landmarks():
+  see_landmarks = input("Would you like to see the list of landmarks again? Enter y/n: ")
+  if see_landmarks == "y":
+    print(landmark_string)
 
+def get_route(start_point, end_point):
+  start_stations = vc_landmarks[start_point]
+  end_stations = vc_landmarks[end_point]
+  routes = []
+  for start_station in start_stations:
+    for end_station in end_stations:
+      route = bfs(vc_metro, start_station, end_station)
+      if route:
+        routes.append(route)
+  shortest_route = min(routes, key=len)
+  return shortest_route
 
+def goodbye():
+  print("Thanks for using SkyRoute!")
+  
 for letter, landmark in landmark_choices.items():
   landmark_string += "{0} - {1}\n".format(letter, landmark)
 
+# print(skyroute())
+# print(get_route('Library Square', 'Vancouver Aquarium'))
 skyroute()
